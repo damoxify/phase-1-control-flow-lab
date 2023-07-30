4ec98d7c3a3290cca6b5e014a6e2564349e87fd1
Adedamola Ajasa <azeemajasa@gmail.com>
2023-07-30 10:49:35 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet) {
+  let greeting;
+
+  if (distanceInFeet <= 400) {
+    greeting = 'This one is on me!';
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (distanceInFeet > 2500) {
+    greeting = 'No can do.';
+  }
+
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  const response =
+    city === 'NYC'
+      ? 'Ok, sounds good.'
+      : 'No go.';
+    
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+function switchOnCharmFromTip(tipAmount) {
+  let response;
+
+  switch (tipAmount) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    case 'thanks for everything':
+      response = 'Bye.';
+      break;
+   }
+   return response;
+  }
